5722f7933d5ea4c4fbbdca23deda41041ba8dd31
Cesar Martinez <km.music92@gmail.com>
2023-08-05 18:37:52 -0500

    add: app main component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
+
 const GifExpertApp = () => {
-  return <div>GifExpertApp</div>;
+  const categoriesList = ["First Category", "second"];
+
+  const [categories, setCategories] = useState(categoriesList);
+
+  const onAddCategory = () => {
+    setCategories(["Categoria", ...categories]);
+  };
+
+  return (
+    <div>
+      <h1>GifExpertApp</h1>
+      <AddCategory setCategories={setCategories} />
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category, index) => (
+          <li key={index}>{category}</li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default GifExpertApp;
